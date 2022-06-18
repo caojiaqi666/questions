a0991f3b813f8269c2d0b8c17ddb8641718c707d
anjiang <cjq374796@digital-engine.com>
2022-06-18 19:32:57 +0800

    0618

--- 每日一题-aj/0616First.ts
+++ 每日一题-aj/0616First.ts
@@ -8,5 +8,12 @@ type arr2 = [3, 2, 1];
 
 type First<T extends any[]> = T extends [] ? never : T[0];
 
+// 第二种解法
+type FirstInfer<T extends unknown[]> = T extends [infer A, ...infer B]
+  ? A
+  : never;
+
 type head1 = First<arr1>; // expected to be 'a'
 type head2 = First<arr2>; // expected to be 3
+
+type head3 = FirstInfer<arr2>; // expected to be 3
